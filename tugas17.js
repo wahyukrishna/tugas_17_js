2b9cd0f29aeee4db6d6d74a8a430d2ffb774f67e
Wahyu Krishna <hanzo180895@gmail.com>
2021-03-05 23:16:09 +0700

    Tugas 17 JS
    Tugas Niomic

---
+++ tugas17.js
@@ -0,0 +1,37 @@
+// var x = "Saya Belajar di sekolah";
+// console.log(x.charCodeAt(3));
+
+// console.log(String.fromCodePoint(97));
+
+// var huruf = "Belajar di Niomic";
+// console.log(huruf.toLowerCase());
+
+// var huruf = "Belajar di Niomic";
+// console.log(huruf.toUpperCase());
+
+// var kata = "Saya ingin makan";
+// console.log(kata.charAt(3));
+
+// var aku = "Aku Pintar Memasak";
+// console.log(aku.substr(4));
+// console.log(aku.substr(3,10));
+// console.log(aku.substr(-3));
+
+const tugas17 = () =>{
+    //tugas soal no.1
+    var kalimat1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+    var kalimat2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual";
+
+    console.log(kalimat1.toLowerCase());
+    console.log(kalimat1.toUpperCase());
+    console.log(kalimat2.toLowerCase());
+    console.log(kalimat2.toUpperCase());
+    console.log("----------------------");
+
+    //tugas soal no.2
+    var kalimat3 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
+
+    console.log(kalimat3.charCodeAt(0)+"-"+kalimat3.charCodeAt(1)+"-"+kalimat3.charCodeAt(2)+"-"+kalimat3.charCodeAt(3)+"-"+kalimat3.charCodeAt(4)+"-"+kalimat3.charCodeAt(5));
+}
+
+tugas17();
